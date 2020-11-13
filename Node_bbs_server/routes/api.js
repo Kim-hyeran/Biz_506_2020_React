0555348f86f2eb2da4757854dc2d5c0e62341fac
Kim-hyeran <hran718@naver.com>
2020-11-13 16:39:16 +0900

    bbs server

--- Node_bbs_server/routes/api.js
+++ Node_bbs_server/routes/api.js
@@ -5,6 +5,7 @@
  */
 const express = require("express")
 const router = express.Router()
+const {bbsDao} = require("../models")
 
 router.get("/", (req, res)=> {
     // res.send("반갑습니다")
@@ -17,7 +18,26 @@ router.get("/bbsList", (req, res)=> {
         {id : 1, writer : "이몽룡", subject : "게시판"},
         {id : 2, writer : "성춘향", subject : "게시판"}
     ]
-    res.json(list)
+    bbsDao.findAll().then((bbsList)=> {
+        // res.json(bbsList)
+        res.redirect("/api/bbsList")
+    })
+    // res.json(list)
+})
+
+router.get("/insert", (req, res)=> {
+    bbsDao.create({
+        b_writer : req.query.writer || "집보내줘",
+        b_date : Date().toString(),
+        b_subject : "게시글 제목",
+        b_content : "게시글 내용"
+    })
+    .then((result)=> {
+        res.json(result)
+    })
+    .catch((err)=> {
+        res.json(err)
+    })
 })
 
 module.exports = router
